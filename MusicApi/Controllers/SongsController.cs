6e42eb86dfa654dbbcbf657e5382d4f3557b92ad
carlson29 <kometacarlson@gmail.com>
2024-11-21 21:53:04 +0000

    making the token to be genrated after login

--- MusicApi/Controllers/SongsController.cs
+++ MusicApi/Controllers/SongsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -23,6 +24,7 @@ namespace MusicApi.Controllers
 
         // GET: api/Songs
         [HttpGet]
+        [Authorize]
         public async Task<ActionResult<IEnumerable<Songs>>> GetSongs()
         {
             return await _context.Songs.ToListAsync();
--- MusicApi/Service/JwtService.cs
+++ MusicApi/Service/JwtService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MusicApi.Models;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -23,37 +24,43 @@ namespace MusicApi.Service
             {
                 return null;
             }
+            
+            var userAccount = await _dbContext.Users.FirstOrDefaultAsync(x => x.User_Name == request.User_Name && x.Password == request.Password);
+            //Console.WriteLine(userAccount.ToString());
 
-            var userAccount =  _dbContext.Users.Any(x => x.User_Name == request.User_Name);
-
-            var issuer = _configuration["JwtConfig:Issuer"];
-            var audience = _configuration["JwtConfig:Audience"];
-            var key = _configuration["JwtConfig:Key"];
-            var tokenValidityMins = _configuration.GetValue<int>("JwtConfig:TokenValidityMins");
-            var tokenExpiryTimeStamp = DateTime.UtcNow.AddMinutes(tokenValidityMins);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
+            if (userAccount != null)
             {
-                Subject = new ClaimsIdentity(new[] { new Claim (JwtRegisteredClaimNames.Name, request.User_Name )
+                
+                var issuer = _configuration["JwtConfig:Issuer"];
+                var audience = _configuration["JwtConfig:Audience"];
+                var key = _configuration["JwtConfig:Key"];
+                var tokenValidityMins = _configuration.GetValue<int>("JwtConfig:TokenValidityMins");
+                var tokenExpiryTimeStamp = DateTime.UtcNow.AddMinutes(tokenValidityMins);
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[] { new Claim (JwtRegisteredClaimNames.Name, request.User_Name )
                 }),
-                Expires = tokenExpiryTimeStamp,
-                Issuer = issuer,
-                Audience = audience,
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
-                SecurityAlgorithms.HmacSha512Signature),
-            };
+                    Expires = tokenExpiryTimeStamp,
+                    Issuer = issuer,
+                    Audience = audience,
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                    SecurityAlgorithms.HmacSha512Signature),
+                };
 
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
-            var accessToken = tokenHandler.WriteToken(securityToken);
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                var accessToken = tokenHandler.WriteToken(securityToken);
 
-            return new LoginResponseModel
-            {
-                AccessToken = accessToken,
-                UserName = request.User_Name,
-                ExpiresIn = (int)tokenExpiryTimeStamp.Subtract(DateTime.UtcNow).TotalSeconds
-            };
+                return new LoginResponseModel
+                {
+                    AccessToken = accessToken,
+                    UserName = request.User_Name,
+                    ExpiresIn = (int)tokenExpiryTimeStamp.Subtract(DateTime.UtcNow).TotalSeconds
+                };
 
+            }
+            return null;
         }
     }
 }
